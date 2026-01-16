fc431136ceb53127f2f23a5a976bf863ff3d580a
Agent006 <chinahchimene@yahoo.com>
2026-01-16 13:24:55 +0100

    big main

---
+++ dependency/main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"fmt"
+
+	"github.com/thesage6/puppy"
+)
+func main(){
+	line1 := puppy.Bark()
+	line2 := puppy.Sit()
+	fmt.Println(line1)
+	fmt.Println(line2)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thesage6/baseline
 
 go 1.25.2
+
+require github.com/thesage6/puppy v0.0.0-20260116092957-be942ad254a9
